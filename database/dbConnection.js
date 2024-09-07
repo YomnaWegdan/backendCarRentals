fae550e56ed6776d1a98b052bab6199b8d1f5f0f
YomnaWegdan <101523679+YomnaWegdan@users.noreply.github.com>
2024-09-08 00:42:15 +0300

    Update dbConnection.js

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'mongoose';
 
-export const dbConnection = async () => await connect(process.env.DB_URL).then(()=>{
+export const dbConnection = async () => await connect(process.env.DB_Online).then(()=>{
     console.log('database connected'); 
 }).catch((err)=>{
     console.log({msg: 'database not connected', err});
