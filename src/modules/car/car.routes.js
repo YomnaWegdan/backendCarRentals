9fa1ebba01ca27795f250dc793f099f10a962fef
YomnaWegdan <101523679+YomnaWegdan@users.noreply.github.com>
2024-09-29 23:35:21 +0300

    Update car.routes.js

--- src/modules/car/car.routes.js
+++ src/modules/car/car.routes.js
@@ -40,28 +40,9 @@ carRouter.put(
 );
 
 carRouter.delete('/:id', auth(['admin']), CC.deleteCar);  // Ensure only admins can delete cars
+carRouter.get('/count',auth(['admin', 'user']), CC.getCarCount);
 
 export default carRouter;
 
 
-// import { Router } from "express";
-// import * as CC from "./car.controller.js"
-// import {auth} from '../../middlewares/auth.js'
-// import * as CV from "./car.validate.js"
-// import {validation} from "../../middlewares/validate.js"
-// import { multerHost , validExtensions} from "../../middlewares/multer.js";
-
-
-
-// const carRouter = Router()
-
-// carRouter.get('/', auth(['user','admin']), CC.getCars);
-// carRouter.get('/:id', auth(['user','admin']),CC.getCarById);
-
-// // Admin routes (protected)
-// carRouter.post('/',  multerHost(validExtensions.image).fields([{name : "image" , maxCount : 1} , {name:"coverImages" , maxCount : 3} ]) ,validation(CV.createCarValidation),  auth(['admin']), CC.createCar);
-// carRouter.put('/:id', multerHost(validExtensions.image).fields([{name : "image" , maxCount : 1} , {name:"coverImages" , maxCount : 3} ]) ,validation(CV.updateCarValidation),  auth(['admin']), CC.updateCar);
-// carRouter.delete('/:id', auth(['admin']), CC.deleteCar);
-
-// export default carRouter;
 
