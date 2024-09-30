b5daad79d585d16ab3114b4c0d8dd6b196d4e372
YomnaWegdan <101523679+YomnaWegdan@users.noreply.github.com>
2024-09-30 22:42:22 +0300

    Update booking.routes.js

--- src/modules/booking/booking.routes.js
+++ src/modules/booking/booking.routes.js
@@ -9,11 +9,12 @@ const bookingRouter = Router();
 
 bookingRouter.post("/", auth(['user', 'admin']), validation(CV.createBookingValidation), bookingController.createBooking); // Add a booking
 bookingRouter.get("/", auth(['user', 'admin']), bookingController.getUserBookings); // Get all bookings for the user
+bookingRouter.get("/count", auth(['admin', 'user']), bookingController.getBookingCount);
+
 bookingRouter.get("/:id", auth(['user', 'admin']), bookingController.getBookingById); // Get a single booking by ID
 bookingRouter.put("/:id", auth(['user', 'admin']), bookingController.updateBooking); // Update a booking by ID
 bookingRouter.delete("/:id", auth(['user', 'admin']), bookingController.deleteBooking); // Delete a booking by ID
 bookingRouter.put("/:id/cancel", auth(['user', 'admin']), bookingController.cancelBooking); // Cancel a booking by ID
-bookingRouter.get("/count", auth(['admin', 'user']), bookingController.getBookingCount);
 
 
 export default bookingRouter;
